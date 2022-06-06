328686edbbab4ff8c314ecf4a5771be235d3927f
intern-william@nodeflux-67 <justin@nodeflux.io>
2022-06-06 03:28:42 +0000

    fix subheader

--- apps/streamlit/section/repository.py
+++ apps/streamlit/section/repository.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
 def repository(section_key):
-    st.subheader('Preprocessing')
+    st.subheader('Repository')
     col = st.columns([3, 1, 1])
     with col[0]: url = st.text_input('Repository URL', key=f'{section_key} input url')
     with col[1]: token = st.text_input('Token', key=f'{section_key} input token')
     with col[2]: tag = st.text_input('Tag/Branch', key=f'{section_key} input tag')
     return url, token, tag
-    
