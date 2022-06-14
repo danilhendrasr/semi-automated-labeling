a1f01cc6514c65d02b40f4464235c324faf940e7
ruhyadi <ruhyadi.dr@gmail.com>
2022-06-14 08:51:07 +0000

    add review cvat function

--- apps/streamlit/function/fiftyone51.py
+++ apps/streamlit/function/fiftyone51.py
@@ -1,109 +1,168 @@
 """ FiftyOne Functions """
 
+from typing import List
 import fiftyone as fo
 import os
+import json
+
+from sklearn import datasets
+
 
 def convert_to_coco(dataset_dir):
-    """ Convert from COCO CVAT format to Fiftyone format """
+    """Convert from COCO CVAT format to Fiftyone format"""
     # rename directory images to data
-    src = os.path.join(dataset_dir, 'images')
-    dst = os.path.join(dataset_dir, 'data')
+    src = os.path.join(dataset_dir, "images")
+    dst = os.path.join(dataset_dir, "data")
     os.rename(src, dst)
 
     # rename instance_default.json to labels.json
-    src_1 = os.path.join(dataset_dir, 'annotations', 'instances_default.json')
-    dst_1 = os.path.join(dataset_dir, 'labels.json')
+    src_1 = os.path.join(dataset_dir, "annotations", "instances_default.json")
+    dst_1 = os.path.join(dataset_dir, "labels.json")
     os.rename(src_1, dst_1)
 
-    print('[INFO] Success convert to Fiftyone COCO Format')
+    print("[INFO] Success convert to Fiftyone COCO Format")
 
-def preview_fiftyone(dataset_name: str, dataset_dir: str, 
-                    delete_existing: bool = True, port: int = 5151):
-    """ Preview dataset to FiftyOne Apps """
+
+def preview_fiftyone(
+    dataset_name: str,
+    dataset_dir: str,
+    delete_existing: bool = True,
+    is_patches: bool = True,
+    port: int = 5151,
+):
+    """Preview dataset to FiftyOne Apps"""
     # delete existing dataset
     if delete_existing:
         list_datasets = fo.list_datasets()
         try:
             [fo.delete_dataset(name) for name in list_datasets]
         except:
-            print('[INFO] No Existing Dataset')
-    
+            print("[INFO] No Existing Dataset")
+
     dataset = fo.Dataset.from_dir(
         dataset_dir=dataset_dir,
         dataset_type=fo.types.COCODetectionDataset,
-        name=dataset_name
+        name=dataset_name,
     )
 
-    fo.launch_app(dataset, address='0.0.0.0', port=port)
+    session = fo.launch_app(dataset, address="0.0.0.0", port=port)
+
+    if is_patches:
+        # view mode patches ground truth/predictions
+        gt_patches = dataset.to_patches("ground_truth")
+        session.view = gt_patches
+
+        return dataset, gt_patches
+    else:
+        session.view = None
+
+        return dataset
+
+
+def save_tags_patches(patches, dataset_dir: str):
+    """ Save fiftyone tags to COCO json dataset format """
+
+    # iterates tags in dataset
+    tags = []
+    for pred in patches:
+        try:
+            tags.append({(pred.ground_truth.tags)[0]: True})
+        except:
+            tags.append(None)
+
+    # load coco json
+    labels_json = json.load(open(os.path.join(dataset_dir, "labels.json")))
+
+    index = []
+    for i, annot in enumerate(labels_json["annotations"]):
+        # change tags according to fiftyone tags
+        if tags[i] is not None:
+            annot["attributes"] = tags[i]
+            index.append(i)
+        else:
+            pass
+
+    # save coco json
+    with open(os.path.join(dataset_dir, "labels.json"), "w") as f:
+        f.write(json.dumps(labels_json))
+
+    return index
+
 
 def load(dataset_dir, label_tags=None):
-    if label_tags is None: label_tags = []
-    if label_tags == 'all':
+    if label_tags is None:
+        label_tags = []
+    if label_tags == "all":
         label_tags = set()
-        with open(os.path.join(dataset_dir, 'labels.json'), 'r') as f:
+        with open(os.path.join(dataset_dir, "labels.json"), "r") as f:
             data = json.load(f)
-            for annotation in data['annotations']:
-                for label_tag in annotation['attributes']:
+            for annotation in data["annotations"]:
+                for label_tag in annotation["attributes"]:
                     label_tags.add(label_tag)
         label_tags = list(label_tags)
-    
+
     dataset = fo.Dataset.from_dir(
-        dataset_dir=dataset_dir,
-        dataset_type=fo.types.COCODetectionDataset
+        dataset_dir=dataset_dir, dataset_type=fo.types.COCODetectionDataset
     )
-    
+
     for sample in dataset:
         for detection in sample.ground_truth.detections:
             for label_tag in label_tags:
                 if label_tag in detection:
                     detection.tags.append(label_tag)
         sample.save()
-        
+
     return dataset
 
+
 def save(dataset, labels_path, label_tags=None):
-    if label_tags is None: label_tags = []
-    if label_tags == 'all':
+    if label_tags is None:
+        label_tags = []
+    if label_tags == "all":
         label_tags = set()
         for sample in dataset:
             for detection in sample.ground_truth.detections:
                 for label_tag in detection.tags:
                     label_tags.add(label_tag)
         label_tags = list(label_tags)
-    
+
     for sample in dataset:
         for detection in sample.ground_truth.detections:
             for label_tag in label_tags:
                 if label_tag in detection.tags:
                     detection[label_tag] = True
         sample.save()
-    
+
     dataset.export(
         dataset_type=fo.types.COCODetectionDataset,
         labels_path=labels_path,
-        label_field='ground_truth',
+        label_field="ground_truth",
     )
-    
-    with open(labels_path, 'r') as f:
+
+    with open(labels_path, "r") as f:
         data = json.load(f)
-    
-    for i, annotation in enumerate(data['annotations']):
-        annotation['attributes'] = dict()
+
+    for i, annotation in enumerate(data["annotations"]):
+        annotation["attributes"] = dict()
         for label_tag in label_tags:
-            if label_tag in annotation: annotation['attributes'][label_tag] = annotation[label_tag]
-        data['annotations'][i] = annotation
-    
-    with open(labels_path, 'w') as f:
+            if label_tag in annotation:
+                annotation["attributes"][label_tag] = annotation[label_tag]
+        data["annotations"][i] = annotation
+
+    with open(labels_path, "w") as f:
         json.dump(data, f)
+
 
 def filter_tags(dataset, label_tags):
     idx = []
     for i, sample in enumerate(dataset):
         found = False
         for detection in sample.ground_truth.detections:
-            if found: break
+            if found:
+                break
             for label_tag in label_tags:
-                if found: break
+                if found:
+                    break
                 if label_tag in detection.tags:
                     idx.append(i)
                     found = True
--- apps/streamlit/page/post_annotations.py
+++ apps/streamlit/page/post_annotations.py
@@ -2,6 +2,7 @@
 
 import os
 import streamlit as st
+import streamlit.components.v1 as components
 from section import cvat as sec_cvat
 
 from function import fiftyone51
@@ -19,6 +20,46 @@ def post_annotations(
     )
     dataset_dir = os.path.join(dump_dir, task_id)
 
+    # TODO: change to section
+    st.subheader("Review in CVAT")
+    review_btn = st.button("Review in CVAT")
+
+    # init ids
+    if "list_ids" not in st.session_state:
+        st.session_state.list_ids = []
+
+    if "index" not in st.session_state:
+        st.session_state.index = 0
+
+    if "patches" not in st.session_state:
+        st.session_state.patches = None
+
+    if "dataset" not in st.session_state:
+        st.session_state.dataset = None
+
+    col = st.columns([10, 1, 2, 1, 10])
+
+    with col[1]:
+        prev_btn = st.button("Prev")
+    with col[2]:
+        ids_view = st.selectbox(label="Frame Id", options=st.session_state.list_ids)
+    with col[3]:
+        next_btn = st.button("Next")
+
+    if prev_btn:
+        components.iframe(
+            src=f"http://192.168.103.67:8080/tasks/39/jobs/18?frame={st.session_state.list_ids[st.session_state.index]}",
+            height=800,
+        )
+        st.session_state.index -= 1
+
+    if next_btn:
+        components.iframe(
+            src=f"http://192.168.103.67:8080/tasks/39/jobs/18?frame={st.session_state.list_ids[st.session_state.index]}",
+            height=800,
+        )
+        st.session_state.index += 1
+
     if btn:
         dataset = cvat.CVAT(
             username=username,
@@ -26,6 +67,7 @@ def post_annotations(
             host="http://192.168.103.67:8080",
             dump_dir=dump_dir,
         )
+        st.session_state.dataset = dataset
 
         dataset.tasks_dump(
             task_id=task_id,
@@ -33,7 +75,7 @@ def post_annotations(
             filename=f"{task_id}.zip",
             extract=True,
         )
-        st.success(f'[INFO] Download Dataset Task {task_id}')
+        st.success(f"[INFO] Download Dataset Task {task_id}")
 
         # apply nms
         # TODO: maybe create another page/section
@@ -48,15 +90,29 @@ def post_annotations(
             iou_threshold=float(iou_thres),
             dump_json=True,
         )
-        st.success('[INFO] Apply NMS')
+        st.success("[INFO] Apply NMS")
 
         fiftyone51.convert_to_coco(dataset_dir=dataset_dir)
-        st.success('[INFO] Convert to FiftyOne COCO Format')
+        st.success("[INFO] Convert to FiftyOne COCO Format")
 
-        fiftyone51.preview_fiftyone(
+        dataset_51, patches = fiftyone51.preview_fiftyone(
             dataset_name=task_id,
             dataset_dir=dataset_dir,
             delete_existing=True,
             port=port,
         )
-        st.success('[INFO] Load to FiftyOne')
+        st.session_state.patches = patches
+        st.success("[INFO] Load to FiftyOne")
+
+    if review_btn:
+        index = fiftyone51.save_tags_patches(
+            patches=st.session_state.patches, dataset_dir=dataset_dir
+        )
+        st.session_state.list_ids.append(index)
+
+        # upload to cvat
+        st.session_state.dataset.tasks_upload(
+            task_id=task_id,
+            fileformat="COCO 1.0",
+            filename=os.path.join(dataset_dir, "labels.json"),
+        )
